b27daad3cf058b76dc27568b1a76770a570facdd
Matt Berk <matthew.p.berk@inte.com>
2012-08-02 13:54:21 -0700

    Added notification.js

---
+++ src/cordova-win8/js/notification.js
@@ -0,0 +1,27 @@
+﻿/**
+ * This class provides access to the notification code.
+ */
+function Notification() { };
+
+Notification.prototype.alert = function (message, alertCallback, title, buttonName) {
+    title = title || "Alert";
+    buttonName = buttonName || "OK";
+
+    var md = new Windows.UI.Popups.MessageDialog(message, title);
+    md.commands.append(new Windows.UI.Popups.UICommand(buttonName));
+    md.showAsync().then(alertCallback);
+};
+Notification.prototype.confirm = function (message, confirmCallback, title, buttonLabels) {
+    title = title || "Confirm";
+    buttonLabels = buttonLabels || "OK,Cancel";
+
+    var md = new Windows.UI.Popups.MessageDialog(message, title);
+    var button = buttonLabels.split(',');
+    md.commands.append(new Windows.UI.Popups.UICommand(button[0]));
+    md.commands.append(new Windows.UI.Popups.UICommand(button[1]));
+    md.showAsync().then(confirmCallback);
+};
+
+if (typeof navigator.notification == "undefined") {
+    navigator.notification = new Notification;
+}
