ca106cfd91efa74b3556d70b6a936e77d85ec169
Jesse MacFadyen <purplecabbage@gmail.com>
2014-02-26 17:52:17 -0800

    CB-2970 CB-2953 log unsupported methods and exit with code 1

---
+++ windows8/template/cordova/lib/list-emulator-images.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO Error! Windows 8 Cordova CLI tools do not support list-emulator-images currently.
+EXIT /B 1
---
+++ windows8/template/cordova/lib/list-started-emulators.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO Error! Windows 8 Cordova CLI tools do not support list-started-emulators currently.
+EXIT /B 1
