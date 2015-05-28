6a27c27a569ee63894477d58d9593e4bad191e95
Vladimir Kotikov <v-vlkoti@microsoft.com>
2015-05-28 16:20:08 +0300

    Removes PlatformHandler class and expose its methods directly through PlatformApi

--- template/cordova/Api.js
+++ template/cordova/Api.js
@@ -28,27 +28,12 @@ PlatformApi.prototype.requirements = function () {
     return requirementsImpl.check_all();
 };
 
-function PlatformHandler() {
-    // Set up basic properties. They probably will be overridden if this API is used by cordova-lib
-    this.root = path.join(__dirname, '..', '..');
-    this.platform = 'windows';
-
-    if (this.constructor.super_){
-        // This should only happen if this class is being instantiated from cordova-lib
-        // In this case the arguments is being passed from cordova-lib as well,
-        // so we don't need to care about whether they're correct ot not
-        this.constructor.super_.apply(this, arguments);
-    }
-}
-
-PlatformHandler.prototype.config_xml = function() {
+PlatformApi.prototype.getConfigXml = function() {
     return path.join(this.root, 'config.xml');
 };
 
-PlatformHandler.prototype.cordovajs_path = function() {
-    return path.join(this.root, 'www', 'cordova.js');
+PlatformApi.prototype.getWwwDir = function() {
+    return path.join(this.root, 'www');
 };
-
-PlatformApi.PlatformHandler = PlatformHandler;
 
 module.exports = PlatformApi;
