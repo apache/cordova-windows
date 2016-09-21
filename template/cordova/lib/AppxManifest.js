a879ad8a879a20e71bba1992395dd8aa994fc33f
Steffen Schaffert <schaffert@itc.rwth-aachen.de>
2016-09-21 11:21:43 +0200

    Removed ForegroundText argument check

--- template/cordova/lib/AppxManifest.js
+++ template/cordova/lib/AppxManifest.js
@@ -390,11 +390,6 @@ AppxManifest.prototype.getVisualElements = function () {
         },
         setForegroundText: function (color) {
             if (color) {
-                // Foreground color can either be dark or light, light is the default
-                if (color !== 'dark') {
-                    color = 'light';
-                }
-
                 visualElements.attrib.ForegroundText = color;
             }
             
