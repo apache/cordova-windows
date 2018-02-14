fd610872a3515e7b4cd99597f06d0a16bc103a18
Jan Piotrowski <piotrowski@gmail.com>
2018-02-14 19:15:20 +0100

    clearer comment

--- template/cordova/lib/MSBuildTools.js
+++ template/cordova/lib/MSBuildTools.js
@@ -89,16 +89,17 @@ module.exports.findAvailableVersion = function () {
 // build.js -> run()
 // check_reqs.js -> checkMSBuild()
 module.exports.findAllAvailableVersions = function () {
-    // CB-11548 use VSINSTALLDIR environment if defined to find MSBuild. If VSINSTALLDIR
-    // is not specified or doesn't contain the MSBuild path we are looking for - fall back
-    // to default discovery mechanism.
     console.log('findAllAvailableVersions');
+    // CB-11548 use VSINSTALLDIR environment if defined to find MSBuild. 
     if (process.env.VSINSTALLDIR) {
         var msBuildPath = path.join(process.env.VSINSTALLDIR, 'MSBuild/15.0/Bin');
         return module.exports.getMSBuildToolsAt(msBuildPath)
             .then(function (msBuildTools) {
                 return [msBuildTools];
-            }).catch(findAllAvailableVersionsFallBack);
+            })
+            // If VSINSTALLDIR is not specified or doesn't contain the MSBuild path we are 
+            // looking for - fall back to default discovery mechanism.
+            .catch(findAllAvailableVersionsFallBack);
     }
 
     return findAllAvailableVersionsFallBack();
