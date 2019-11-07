cba6a2bc6363acec23d05968e42044f989fa8d5d
Raphael von der Grün <raphinesse@gmail.com>
2019-11-07 11:19:12 +0100

    Lint everything (#368)
    * Lint everything, including bins w/out extension
    
    * Apply eslint --fix to all linted files
    
    * Manually fix all remaining lint rule violations

--- cordova-js-src/confighelper.js
+++ cordova-js-src/confighelper.js
@@ -21,13 +21,13 @@
 
 // config.xml and AppxManifest.xml wrapper (non-node ConfigParser analogue)
 var configCache = {};
-var utils = require("cordova/utils");
+var utils = require('cordova/utils');
 
-var isPhone = (cordova.platformId == 'windows') && WinJS.Utilities.isPhone;
+var isPhone = (cordova.platformId === 'windows') && WinJS.Utilities.isPhone;
 var isWin10UWP = navigator.appVersion.indexOf('MSAppHost/3.0') !== -1;
-var splashScreenTagName = isWin10UWP ? "SplashScreen" : (isPhone ? "m3:SplashScreen" : "m2:SplashScreen");
+var splashScreenTagName = isWin10UWP ? 'SplashScreen' : (isPhone ? 'm3:SplashScreen' : 'm2:SplashScreen');
 
-function XmlFile(text) {
+function XmlFile (text) {
     this.text = text;
 }
 
@@ -35,19 +35,19 @@ XmlFile.prototype.loadTags = function (tagName) {
     var parser;
     if (!this.doc) {
         parser = new DOMParser();
-        this.doc = parser.parseFromString(this.text, "application/xml");
+        this.doc = parser.parseFromString(this.text, 'application/xml');
     }
 
     var tags = this.doc.getElementsByTagName(tagName);
     return Array.prototype.slice.call(tags);
-}
+};
 
-function Config(text) {
+function Config (text) {
     XmlFile.apply(this, arguments);
-    this.preferences = this.loadTags("preference");
+    this.preferences = this.loadTags('preference');
 }
 
-function Manifest(text) {
+function Manifest (text) {
     XmlFile.apply(this, arguments);
     this.splashScreen = this.loadTags(splashScreenTagName)[0];
 }
@@ -55,14 +55,14 @@ function Manifest(text) {
 utils.extend(Config, XmlFile);
 utils.extend(Manifest, XmlFile);
 
-function requestFile(filePath, success, error) {
+function requestFile (filePath, success, error) {
     var xhr;
 
-    if (typeof configCache[filePath] != 'undefined') {
+    if (typeof configCache[filePath] !== 'undefined') {
         success(configCache[filePath]);
     }
 
-    function fail(msg) {
+    function fail (msg) {
         console.error(msg);
 
         if (error) {
@@ -71,36 +71,35 @@ function requestFile(filePath, success, error) {
     }
 
     var xhrStatusChangeHandler = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200 || xhr.status == 304 || xhr.status == 0 /* file:// */) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200 || xhr.status === 304 || xhr.status === 0 /* file:// */) {
                 configCache[filePath] = xhr.responseText;
                 success(xhr.responseText);
-            }
-            else {
+            } else {
                 fail('[Windows][cordova.js][xhrStatusChangeHandler] Could not XHR ' + filePath + ': ' + xhr.statusText);
             }
         }
     };
 
     xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", xhrStatusChangeHandler);
+    xhr.addEventListener('load', xhrStatusChangeHandler);
 
     try {
-        xhr.open("get", filePath, true);
+        xhr.open('get', filePath, true);
         xhr.send();
     } catch (e) {
         fail('[Windows][cordova.js][xhrFile] Could not XHR ' + filePath + ': ' + JSON.stringify(e));
     }
 }
 
-function readConfig(success, error) {
-    requestFile("/config.xml", function (contents) {
+function readConfig (success, error) {
+    requestFile('/config.xml', function (contents) {
         success(new Config(contents));
     }, error);
 }
 
-function readManifest(success, error) {
-    requestFile("/AppxManifest.xml", function (contents) {
+function readManifest (success, error) {
+    requestFile('/AppxManifest.xml', function (contents) {
         success(new Manifest(contents));
     }, error);
 }
@@ -117,14 +116,14 @@ Config.prototype.getPreferenceValue = function (preferenceName) {
     if (preferenceItem && preferenceItem[0] && preferenceItem[0].attributes && preferenceItem[0].attributes['value']) {
         return preferenceItem[0].attributes['value'].value;
     }
-}
+};
 
 /**
  * Reads SplashScreen image path
  */
 Manifest.prototype.getSplashScreenImagePath = function () {
     return this.splashScreen.attributes['Image'].value;
-}
+};
 
 exports.readConfig = readConfig;
 exports.readManifest = readManifest;
